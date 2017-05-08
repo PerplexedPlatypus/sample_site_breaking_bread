a976873ecb6754a88f9a906b07fb069770e6a8cf
Miles Peiser <partially.perplexed.platypus@gmail.com>
2017-05-08 09:35:42 -0400

    removed corrupt image file from array

--- sample_site/js/script.js
+++ sample_site/js/script.js
@@ -1,7 +1,7 @@
 
 var myImage = document.getElementById("communityPics");
 
-var imageArray = ["images/healthy-vegetables-restaurant-nature.jpg", "images/bread.jpg","images/eggplants.jpg","images/garden.jpg", "images/garden_2.jpg","images/herb-wall.jpg", "images/leaves.jpg", "images/harshal-hirve-44494.jpg", "images/peter-wendt-123928.jpg", "images/nachos.jpg", "images/irina-blok-13584.jpg", "images/food-81981.jpg", "images/healthy-vegetables-restaurant-nature.jpg"];
+var imageArray = ["images/healthy-vegetables-restaurant-nature.jpg", "images/bread.jpg","images/eggplants.jpg","images/garden.jpg", "images/garden_2.jpg","images/herb-wall.jpg", "images/leaves.jpg", "images/harshal-hirve-44494.jpg", "images/nachos.jpg", "images/irina-blok-13584.jpg", "images/food-81981.jpg"];
 var imageIndex = 0;
 //set image src to Array index
 function changeImage() {
