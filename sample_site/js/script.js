870847e54fe631ab320ea16f2ea874239f8ddee6
PerplexedPlatypus <partially.perplexed.platypus@gmail.com>
2017-05-08 01:49:28 -0400

    First commit

---
+++ sample_site/js/script.js
@@ -0,0 +1,23 @@
+
+var myImage = document.getElementById("communityPics");
+
+var imageArray = ["images/healthy-vegetables-restaurant-nature.jpg", "images/bread.jpg","images/eggplants.jpg","images/garden.jpg", "images/garden_2.jpg","images/herb-wall.jpg", "images/leaves.jpg", "images/harshal-hirve-44494.jpg", "images/peter-wendt-123928.jpg", "images/nachos.jpg", "images/irina-blok-13584.jpg", "images/food-81981.jpg"];
+var imageIndex = 0;
+//set image src to Array index
+function changeImage() {
+    myImage.setAttribute("src",imageArray[imageIndex]);
+    imageIndex++;
+    if (imageIndex >= imageArray.length) {
+        imageIndex = 0;
+    }
+}
+
+// setInterval 5 seconds
+var intervalInterrupt = setInterval(changeImage,3000);
+myImage.onclick() = function() {
+  clearInterval(intervalInterrupt);
+}
+
+
+//add validation for forms
+//add events for active and blur on forms
