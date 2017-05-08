1e6d6d29245d2a486b338ef05f38d0e125edba0f
Miles Peiser <partially.perplexed.platypus@gmail.com>
2017-05-08 09:32:55 -0400

    added image link to array
    healthy-vegetables-restaurant-nature.jpg added to imageArray

--- sample_site/js/script.js
+++ sample_site/js/script.js
@@ -1,7 +1,7 @@
 
 var myImage = document.getElementById("communityPics");
 
-var imageArray = ["images/healthy-vegetables-restaurant-nature.jpg", "images/bread.jpg","images/eggplants.jpg","images/garden.jpg", "images/garden_2.jpg","images/herb-wall.jpg", "images/leaves.jpg", "images/harshal-hirve-44494.jpg", "images/peter-wendt-123928.jpg", "images/nachos.jpg", "images/irina-blok-13584.jpg", "images/food-81981.jpg"];
+var imageArray = ["images/healthy-vegetables-restaurant-nature.jpg", "images/bread.jpg","images/eggplants.jpg","images/garden.jpg", "images/garden_2.jpg","images/herb-wall.jpg", "images/leaves.jpg", "images/harshal-hirve-44494.jpg", "images/peter-wendt-123928.jpg", "images/nachos.jpg", "images/irina-blok-13584.jpg", "images/food-81981.jpg", "images/healthy-vegetables-restaurant-nature.jpg"];
 var imageIndex = 0;
 //set image src to Array index
 function changeImage() {
@@ -13,10 +13,7 @@ function changeImage() {
 }
 
 // setInterval 5 seconds
-var intervalInterrupt = setInterval(changeImage,3000);
-myImage.onclick() = function() {
-  clearInterval(intervalInterrupt);
-}
+var intervalInterrupt = setInterval(changeImage,4000);
 
 
 //add validation for forms
